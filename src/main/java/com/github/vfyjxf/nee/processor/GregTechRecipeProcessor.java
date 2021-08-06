0201f20973c64399ad6f2225f9a2917f2532bb5f
GlodBlock <60341015+GlodBlock@users.noreply.github.com>
2021-08-06 22:00:02 +0800

    阻止闪存编程电路板之类的不消耗物品被加入

--- src/main/java/com/github/vfyjxf/nee/processor/GregTechRecipeProcessor.java
+++ src/main/java/com/github/vfyjxf/nee/processor/GregTechRecipeProcessor.java
@@ -2,13 +2,8 @@ package com.github.vfyjxf.nee.processor;
 
 import codechicken.nei.PositionedStack;
 import codechicken.nei.recipe.IRecipeHandler;
-import gregtech.api.enums.ItemList;
 import gregtech.api.util.GT_Recipe;
 import gregtech.api.util.GT_Utility;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidRegistry;
-import net.minecraftforge.fluids.FluidStack;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -52,7 +47,7 @@ public class GregTechRecipeProcessor implements IRecipeProcessor {
     public List<PositionedStack> getRecipeInput(IRecipeHandler recipe, int recipeIndex, String identifier) {
         if (gtDefaultClz.isInstance(recipe) || gtAssLineClz.isInstance(recipe)) {
             List<PositionedStack> recipeInputs = new ArrayList<>(recipe.getIngredientStacks(recipeIndex));
-            recipeInputs.removeIf(positionedStack -> GT_Utility.getFluidFromDisplayStack(positionedStack.items[0]) != null);
+            recipeInputs.removeIf(positionedStack -> GT_Utility.getFluidFromDisplayStack(positionedStack.items[0]) != null || positionedStack.item.stackSize == 0);
             return recipeInputs;
         }
         return null;
